6af64d1f9b2f2851549b3aa3b7091b9b2983e2fe
Felipe Palma <felipepalma14@gmail.com>
2025-10-26 21:58:08 -0400

    Initial commit - Jacoco Intro

---
+++ app/src/main/java/com/felipepalma14/jacocointro/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.felipepalma14.jacocointro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.felipepalma14.jacocointro.ui.theme.JacocoIntroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JacocoIntroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(text = "Jacoco intro!",
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}

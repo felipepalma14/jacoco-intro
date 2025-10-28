3736e95ab34a43b0dabf3c7c816a25eac3f9ba4d
Felipe Palma <felipepalma14@gmail.com>
2025-10-27 21:44:47 -0400

    Add unit test

--- app/src/test/java/com/felipepalma14/jacocointro/data/FakeRepositoryImplTest.kt
+++ app/src/test/java/com/felipepalma14/jacocointro/data/FakeRepositoryImplTest.kt
@@ -1,15 +1,11 @@
 package com.felipepalma14.jacocointro.data
 
 import com.felipepalma14.jacocointro.domain.IRepository
-import io.mockk.mockk
-import org.junit.Before
 import org.junit.Test
 
 class FakeRepositoryImplTest {
 
     val repository: IRepository = FakeRepositoryImpl()
-
-
 
     @Test
     fun testGetData() {
